9db4abd414b8370816354968fe86097e0163028c
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-27 16:53:30 -0600

    Add serializers

---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,13 @@
+class ItemSerializer
+  def self.format_items(items)
+    items.map do |item|
+      {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        unit_price: item.unit_price,
+        merchant_id: item.merchant_id
+      }
+    end
+  end
+end
