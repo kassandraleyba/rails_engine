034812fa7efefb64eb87e6bf213625fa7ce8e0fa
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-27 14:43:07 -0600

    Add api endpoint merchant show /merchant/:id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,4 +18,24 @@ describe "Merchants API" do
       expect(merchant[:name]).to be_a(String)
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
+
+  it "can get all items for a given merchant ID" do
+    
+  end
 end
