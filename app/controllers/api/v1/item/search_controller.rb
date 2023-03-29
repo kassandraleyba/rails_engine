6b4fa385edf906a9a85345b72a3f9d17f23cc4c3
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-29 11:18:43 -0600

    Add searched item name to controller

--- app/controllers/api/v1/item/search_controller.rb
+++ app/controllers/api/v1/item/search_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Item::SearchController < ApplicationController
     # created a route that routes to show controller
     # and allowing this route to pass in params
     # next step is to create the class method in the model to find the item name
-    
-    # render json: ItemSerializer.new(Item.find_by([:name]))
+    item = Item.search_by_name(params[:name])
+    render json: ItemSerializer.new(item)
   end
 end
